4cebea863a89f8459fedd3936ddf988744defb07
brianadiyatma <72308379+brianadiyatma@users.noreply.github.com>
2022-03-28 21:21:30 +0700

    Awal dari Project

---
+++ src/Components/Burger.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Bun from "./Bun";
+import Patty from "./Patty";
+import Tomato from "./Tomato";
+import Cheese from "./Cheese";
+import Lettuce from "./Lettuce";
+
+const Burger = () => {
+  return (
+    <>
+      <Bun />
+      <Patty />
+      <Tomato />
+      <Cheese />
+      <Lettuce />
+    </>
+  );
+};
+
+export default Burger;
